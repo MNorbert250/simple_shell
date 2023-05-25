9adf7c08c6f6012c2c33aa3ea4b3003840bb5fc7
nshuti17 <nshutimartial17@gmail.com>
2023-05-24 23:51:34 -0700

    errors1.c

--- errors1.c
+++ errors1.c
@@ -2,20 +2,20 @@
 
 /**
  * _erratoi - converts a string to an integer
- * @m: the string to be converted
+ * @s: the string to be converted
  * Return: 0 if no numbers in string, converted number otherwise
  *       -1 on error
  */
-int _erratoi(char *m)
+int _erratoi(char *s)
 {
 	int i = 0;
 	unsigned long int result = 0;
 
-	if (*m == '+')
-		m++;  /* TODO: why does this make main return 255? */
-	for (i = 0;  m[i] != '\0'; i++)
+	if (*s == '+')
+		s++;  /* TODO: why does this make main return 255? */
+	for (i = 0;  s[i] != '\0'; i++)
 	{
-		if (m[i] >= '0' && m[i] <= '9')
+		if (s[i] >= '0' && s[i] <= '9')
 		{
 			result *= 10;
 			result += (s[i] - '0');
@@ -30,20 +30,20 @@ int _erratoi(char *m)
 
 /**
  * print_error - prints an error message
- * @news: the parameter & return news struct
- * @ptn: string containing specified error type
+ * @info: the parameter & return info struct
+ * @estr: string containing specified error type
  * Return: 0 if no numbers in string, converted number otherwise
  *        -1 on error
  */
-void print_error(news_t *news, char *ptn)
+void print_error(info_t *info, char *estr)
 {
-	_eputs(news->fname);
+	_eputs(info->fname);
 	_eputs(": ");
-	print_d(news->line_count, STDERR_FILENO);
+	print_d(info->line_count, STDERR_FILENO);
 	_eputs(": ");
-	_eputs(news->argv[0]);
+	_eputs(info->argv[0]);
 	_eputs(": ");
-	_eputs(ptn);
+	_eputs(estr);
 }
 
 /**
@@ -138,4 +138,3 @@ void remove_comments(char *buf)
 			break;
 		}
 }
-
